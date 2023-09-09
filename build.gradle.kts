6c5628dda228c3eb92154e677fb423c8e31c612f
Nikita Klyshko <n-klyshko@mail.ru>
2023-09-09 22:36:14 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.gradle.configurationcache.extensions.capitalized
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "io.cerebus.customnpcs"
+version = "1.0-SNAPSHOT"
+
+val mojangMapped = project.hasProperty("mojang-mapped")
+
+plugins {
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.nova)
+    alias(libs.plugins.stringremapper)
+    alias(libs.plugins.specialsource)
+}
+
+dependencies {
+    implementation(libs.nova)
+}
+
+addon {
+    id.set(project.name)
+    name.set(project.name.capitalized())
+    version.set(project.version.toString())
+    novaVersion.set(libs.versions.nova)
+    main.set("io.cerebus.customnpcs.CustomNPCs")
+    authors.add("CraftCoderr")
+}
+
+spigotRemap {
+    spigotVersion.set(libs.versions.spigot.get().substringBefore('-'))
+    sourceJarTask.set(tasks.jar)
+}
+
+remapStrings {
+    remapGoal.set(if (mojangMapped) "mojang" else "spigot")
+    spigotVersion.set(libs.versions.spigot.get())
+}
+
+tasks {
+    register<Copy>("addonJar") {
+        group = "build"
+        dependsOn("addon", if (mojangMapped) "jar" else "remapObfToSpigot")
+        
+        from(File(project.buildDir, "libs/${project.name}-${project.version}.jar"))
+        into((project.findProperty("outDir") as? String)?.let(::File) ?: project.buildDir)
+        rename { it.replace(project.name, addon.get().addonName.get()) }
+    }
+    
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
---
+++ src/main/kotlin/io/cerebus/customnpcs/CustomNPCs.kt
@@ -0,0 +1,19 @@
+package io.cerebus.customnpcs
+
+import xyz.xenondevs.nova.addon.Addon
+
+object CustomNPCs : Addon() {
+    
+    override fun init() {
+        // Called when the addon is initialized.
+    }
+    
+    override fun onEnable() {
+        // Called when the addon is enabled.
+    }
+    
+    override fun onDisable() {
+        // Called when the addon is disabled.
+    }
+    
+}
